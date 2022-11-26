97728af9201937b6e65c3d2b1375c3f9c430dffc
MaxCode2023 <course.ios@artsofte.ru>
2022-11-26 13:37:40 +0500

    first commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by macOS on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
