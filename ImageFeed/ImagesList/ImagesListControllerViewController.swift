b81b9f4cc9dde27e51d38fdabd80a5cc5fa05b4f
MaxCode2023 <course.ios@artsofte.ru>
2022-11-28 01:39:07 +0500

    fix

--- ImageFeed/ImagesList/ImagesListControllerViewController.swift
+++ ImageFeed/ImagesList/ImagesListControllerViewController.swift
@@ -11,8 +11,19 @@ class ImagesListViewController: UIViewController {
 
     @IBOutlet private var tableViewImage: UITableView!
     
+    private var photosName = [String]()
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        photosName = Array(0..<20).map{"\($0)"}
     }
 }
 
@@ -23,8 +34,10 @@ extension ImagesListViewController: UITableViewDelegate {
 }
 
 extension ImagesListViewController: UITableViewDataSource {
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -34,11 +47,23 @@ extension ImagesListViewController: UITableViewDataSource {
         guard let imagesListCell = cell as? ImagesListCell else {
             return UITableViewCell()
         }
-        configCell(for: imagesListCell)
+        configCell(for: imagesListCell, with: indexPath)
         
         return imagesListCell
     }
     
-    func configCell(for cell: ImagesListCell) { }
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+        cell.imageCell.image = image
+        cell.date.text = dateFormatter.string(from: Date())
+        
+        if indexPath.row % 2 == 0 {
+            cell.favoriteButton.setImage(UIImage(named: "Active"), for: .normal)
+        } else{
+            cell.favoriteButton.setImage(UIImage(named: "No Active"), for: .normal)
+        }
+    }
 }
 
