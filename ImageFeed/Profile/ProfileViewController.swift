a4316d17f6a4ad421b61c79df711fe8cf10086ec
MaxCode2023 <course.ios@artsofte.ru>
2022-12-21 13:08:12 +0500

    manual layout

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,18 +9,78 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    @IBOutlet weak var exitButton: UIButton!
-    @IBOutlet weak var imageProfile: UIImageView!
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var nickName: UILabel!
-    @IBOutlet weak var status: UILabel!
+//    @IBOutlet weak var exitButton: UIButton!
+//    @IBOutlet weak var imageProfile: UIImageView!
+//    @IBOutlet weak var name: UILabel!
+//    @IBOutlet weak var nickName: UILabel!
+//    @IBOutlet weak var status: UILabel!
+    
+    let image = UIImageView()
+    let labelName = UILabel()
+    let labelNickname = UILabel()
+    let labelStatus = UILabel()
+    let button = UIButton.systemButton(with: UIImage(systemName: "ipad.and.arrow.forward")!, target: ProfileViewController.self, action: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        exitButton.setTitle("", for: .normal)
+        
+        addSubviews()
+        setViewConfiguration()
+        activateConstraints()
     }
+    
+    private func addSubviews() {
+        view.addSubview(image)
+        view.addSubview(labelName)
+        view.addSubview(labelNickname)
+        view.addSubview(labelStatus)
+        view.addSubview(button)
+        
+        image.translatesAutoresizingMaskIntoConstraints = false
+        labelName.translatesAutoresizingMaskIntoConstraints = false
+        labelNickname.translatesAutoresizingMaskIntoConstraints = false
+        labelStatus.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setViewConfiguration() {
+        image.image = UIImage(named: "Photo")
 
-    @IBAction func clickExitButton(_ sender: Any) {
+        
+        labelName.text = "Екатерина Новикова"
+        labelName.textColor = UIColor(named: "YP White")
+        labelName.font = labelName.font.withSize(23)
+        
+        labelNickname.text = "@ekaterina_nov"
+        labelNickname.textColor = UIColor(named: "YP Grey")
+        labelNickname.font = labelNickname.font.withSize(13)
+        
+        labelStatus.text = "Hello, world"
+        labelStatus.textColor = UIColor(named: "YP White")
+        labelStatus.font = labelStatus.font.withSize(13)
+        
+        button.tintColor = UIColor(named: "YP Red")
         
     }
+    
+    private func activateConstraints() {
+        NSLayoutConstraint.activate([
+            image.heightAnchor.constraint(equalToConstant: 70),
+            image.widthAnchor.constraint(equalToConstant: 70),
+            image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            image.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            labelName.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 8),
+            labelName.leadingAnchor.constraint(equalTo: image.leadingAnchor),
+            labelNickname.topAnchor.constraint(equalTo: labelName.bottomAnchor, constant: 8),
+            labelNickname.leadingAnchor.constraint(equalTo: labelName.leadingAnchor),
+            labelStatus.topAnchor.constraint(equalTo: labelNickname.bottomAnchor, constant: 8),
+            labelStatus.leadingAnchor.constraint(equalTo: labelName.leadingAnchor),
+            button.centerYAnchor.constraint(equalTo: image.centerYAnchor),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
+
+//    @IBAction func clickExitButton(_ sender: Any) {
+//
+//    }
 }
