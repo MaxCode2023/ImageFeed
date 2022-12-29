8844eae4be65d34d2b97d77a28e6b412d7957fb8
MaxCode2023 <course.ios@artsofte.ru>
2022-12-29 19:08:56 +0500

    web view

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by macOS on 29.12.2022.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    let segueID = "ShowWebView"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by macOS on 29.12.2022.
+//
+
+import UIKit
+import WebKit
+
+class WebViewViewController: UIViewController {
+
+    @IBOutlet private weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        
+    }
+}
