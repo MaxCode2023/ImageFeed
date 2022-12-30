652821c4e62fc1072553b4640b0bc17cd717910a
MaxCode2023 <course.ios@artsofte.ru>
2022-12-30 18:23:48 +0500

    web view autorize

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 
 class AuthViewController: UIViewController {
 
-    let segueID = "ShowWebView"
+    private let segueID = "ShowWebView"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueID {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(segueID)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
 
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+    
+    
 }
